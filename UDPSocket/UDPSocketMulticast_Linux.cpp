6c4f2162127078d35a1471dff99398f023840149
imaxoi <imaxoi@hotmail.it>
2017-10-02 10:51:46 +0200

    Fix UDP multicast for Linux

--- UDPSocket/UDPSocketMulticast_Linux.cpp
+++ UDPSocket/UDPSocketMulticast_Linux.cpp
@@ -1,5 +1,6 @@
 #ifdef __linux__
 #include "UDPSocketMulticast_Linux.h"
+const uint8_t UDPSocketMulticast_Linux::REFRESH_MULTICAST_SECONDS;
 
 UDPSocketMulticast_Linux::UDPSocketMulticast_Linux( string ip, uint16_t multicastPort ) {
 	this->multicastIp = multicastIp;
@@ -26,8 +27,6 @@ UDPSocketMulticast_Linux::UDPSocketMulticast_Linux( string ip, uint16_t multicas
 
 	//Binding the socket
 	sockaddr_in addr;
-	struct ip_mreq mreq;
-
 	memset( &addr, 0, sizeof( addr ) );
 	addr.sin_family = AF_INET;
 	addr.sin_addr.s_addr = htonl( INADDR_ANY );
@@ -49,12 +48,28 @@ UDPSocketMulticast_Linux::UDPSocketMulticast_Linux( string ip, uint16_t multicas
 	multicastSockaddr.sin_family = AF_INET;
 	inet_pton( AF_INET, "239.0.0.100", &(multicastSockaddr.sin_addr) );
 	multicastSockaddr.sin_port = htons( multicastPort );
+	
+	multicastRefresher = thread( &UDPSocketMulticast_Linux::refreshMulticast, this );
 }
 
 UDPSocketMulticast_Linux::~UDPSocketMulticast_Linux() {
 	if ( !closed )
 		closeSocket();
 }
+
+void UDPSocketMulticast_Linux::refreshMulticast() {
+ 	while ( !closed ) {
+ 		if ( setsockopt( receiveSocket, IPPROTO_IP, IP_DROP_MEMBERSHIP, (char *)&mreq, sizeof( mreq ) ) < 0 ) {
+ 			return;
+		}
+ 		if ( setsockopt( receiveSocket, IPPROTO_IP, IP_ADD_MEMBERSHIP, (char *)&mreq, sizeof( mreq ) ) < 0 ) {
+ 			return;
+ 		}
+ 		unique_lock<mutex> ul( mutex_wait_for_refresh );
+ 		cv_wait_for_refresh.wait_for( ul, std::chrono::seconds( REFRESH_MULTICAST_SECONDS ));
+ 	}
+ }
+
 
 void UDPSocketMulticast_Linux::sendPacket( string message ) {
 	sendto( sendSocket, message.c_str(), message.length(), 0, (struct sockaddr *) &multicastSockaddr, sizeof( multicastSockaddr ) );
@@ -79,10 +94,14 @@ int8_t UDPSocketMulticast_Linux::receivePacket( std::string& message, std::strin
 }
 
 void UDPSocketMulticast_Linux::closeSocket() {
+	closed = true;
+	cv_wait_for_refresh.notify_one();
+	if ( multicastRefresher.joinable() )
+		multicastRefresher.join();
+
 	shutdown( receiveSocket, SHUT_RDWR );
 	close( sendSocket );
 	close( receiveSocket );
-	closed = true;
 }
 
 #endif
--- UDPSocket/UDPSocketMulticast_Linux.h
+++ UDPSocket/UDPSocketMulticast_Linux.h
@@ -14,6 +14,9 @@
 #include <unistd.h>
 #include <stdio.h>
 #include <stdlib.h>
+#include <thread>
+#include <mutex>
+#include <condition_variable>
 
 #include "UDPSocketMulticast_Interface.h"
 using namespace std;
@@ -21,12 +24,22 @@ using namespace std;
 
 class UDPSocketMulticast_Linux : public UDPSocketMulticast_Interface {
 private:
+	static const uint8_t REFRESH_MULTICAST_SECONDS=120;
+	
 	int16_t sendSocket;
 	int16_t receiveSocket;
 
 	sockaddr_in multicastSockaddr;
 	string multicastIp;
 	uint16_t multicastPort;
+	
+	struct ip_mreq mreq;
+	
+	mutex mutex_wait_for_refresh;
+ 	condition_variable cv_wait_for_refresh;
+ 	void refreshMulticast();
+ 	thread multicastRefresher;
+	
 
 	bool closed;
 
